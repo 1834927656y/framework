4ea8d4a4c90a331776a1748aa06deaf011327555
zpt_ylh <62537266+1834927656y@users.noreply.github.com>
2025-11-11 10:47:14 +0800

    测试pr

--- undefinedSystemManagement/src/plugins/i18n.ts
+++ undefinedSystemManagement/src/plugins/i18n.ts
@@ -31,44 +31,5 @@ const chinese = ( key: string) => {
     }).then(res => res.json()).then(res => res.trans_result[0].dst)
 }
 
-const mergeTranslatedMessage = (locale: string, key: string, value: string) => {
-  const segments = key.split('.')
-  const last = segments.pop()
-  if (!last) return
-
-  const localeMessage = { ...i18n.global.getLocaleMessage(locale) }
-  let cursor: Record<string, any> = localeMessage
-
-  segments.forEach(segment => {
-    cursor[segment] = cursor[segment] ?? {}
-    cursor = cursor[segment]
-  })
-
-  cursor[last] = value
-  i18n.global.setLocaleMessage(locale, localeMessage)
-}
-
-const composer = i18n.global as typeof i18n.global & {
-  ts: (key: string) => Promise<string>
-}
-
-composer.ts = async (key: string) => {
-  if (composer.te(key)) {
-    return composer.t(key)
-  }
-
-  const locale = composer.locale.value
-  const translated =
-    locale === 'zhCN'
-      ? await chinese(key)
-      : await english(key)
-
-  console.log(translated);
-  
-
-  // mergeTranslatedMessage(locale, key, translated)
-
-  return composer.t(key)
-}
 
 export default i18n
